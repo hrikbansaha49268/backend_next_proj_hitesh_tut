55336afd17a0a93dc0c01f5b6131f4b700bce5c0
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2024-04-23 16:37:50 +0530

    DB connected

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,23 @@
+import mongoose, { MongooseError } from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already DB is connected");
+    } else {
+        try {
+            const db = await mongoose.connect(process.env.MONGO_URI || '', {});
+            connection.isConnected = db.connections[0].readyState;
+            console.log("DB connected succesfully");
+        } catch (error) {
+            throw new MongooseError("Couldn't connect to database");
+        };
+    };
+};
+
+export default dbConnect;
